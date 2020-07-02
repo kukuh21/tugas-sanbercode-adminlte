4c76620651e5059384d9bd9d99d52824f59db6ae
Kukuh Aprianto <kaprianto@gmail.com>
2020-07-02 08:31:47 +0700

    first commit

---
+++ resources/views/layouts/module/sidebar.blade.php
@@ -0,0 +1,32 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <!-- Brand Logo -->
+  <a href="index3.html" class="brand-link">
+      <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="Sanber" class="brand-image img-circle elevation-3"
+          style="opacity: .8">
+      <span class="brand-text font-weight-light">SanberCode</span>
+  </a>
+
+  <!-- Sidebar -->
+  <div class="sidebar">
+      <nav class="mt-2">
+          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+              <li class="nav-item has-treeview menu-open">
+                  <a href="#" class="nav-link active">
+                      <i class="nav-icon fa fa-dashboard"></i>
+                      <p>
+                          Dashboard
+                      </p>
+                  </a>
+              </li>
+
+              <li class="nav-item has-treeview">
+                <a class="nav-link" href="#">
+                    <p>
+                        Logout
+                    </p>
+                </a>
+              </li>
+          </ul>
+      </nav>
+  </div>
+</aside>
